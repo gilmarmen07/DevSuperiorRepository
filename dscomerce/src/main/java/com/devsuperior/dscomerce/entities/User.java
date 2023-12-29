969cce21d7416036ec9c3c2e8347406cd0a28f10
GILMAR-NOTEBOOK\GILMAR <gilmar.mendes@bol.com.br>
2023-12-29 13:41:14 -0300

    DSComerce - First commit

--- dscomerce/src/main/java/com/devsuperior/dscomerce/entities/User.java
+++ dscomerce/src/main/java/com/devsuperior/dscomerce/entities/User.java
@@ -1,4 +1,27 @@
 package com.devsuperior.dscomerce.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+    private String password;
 }
