c35fc1108e34ebe17c1f1417f834a00d14137593
GILMAR-NOTEBOOK\GILMAR <gilmar.mendes@bol.com.br>
2023-12-29 16:19:32 -0300

    DSComerce: Payment, relacionamento um-para-um

--- dscomerce/src/main/java/com/devsuperior/dscomerce/entities/Payment.java
+++ dscomerce/src/main/java/com/devsuperior/dscomerce/entities/Payment.java
@@ -1,6 +1,5 @@
 package com.devsuperior.dscomerce.entities;
 
-import com.devsuperior.dscomerce.enuns.OrderStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,8 +13,8 @@ import java.time.Instant;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "tb_order")
-public class Order {
+@Table(name = "tb_payment")
+public class Payment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,12 +22,8 @@ public class Order {
 
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
-    private OrderStatus status;
 
-    @ManyToOne
-    @JoinColumn(name = "client_id")
-    private User client;
-
-    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
-    private Payment payment;
+    @OneToOne
+    @MapsId
+    private Order order;
 }
